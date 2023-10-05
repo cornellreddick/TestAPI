9a4d95bf8400b4f98d2819abbf85f5d01d2a295f
Cornell Reddick <cornellreddick@outlook.com>
2023-10-05 15:11:28 -0400

    Initial Commit. Testing api

---
+++ TestRestAPI/src/main/java/com/test/api/ApiController.java
@@ -0,0 +1,17 @@
+package com.test.api;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+	
+	@GetMapping("/api/test")
+	public String test() {
+		return "This api is created to refresh on REST API. The current time is: "
+				+ new Date();
+	}
+}
